69d8563025de8bb31cc9f69e458ca14a3561e6e8
IIIAnthonyIII <anthony.delvalle@doriantrade.com>
2024-06-10 15:00:15 -0500

    cambios de estilos

--- src/app/components/shared/navside-works/navside-works.component.ts
+++ src/app/components/shared/navside-works/navside-works.component.ts
@@ -8,106 +8,93 @@ export interface Modulo {
   icono: string;
   permiso: string;
   description: string;
-  state: number
+  state: number;
 }
-
 
 @Component({
   selector: 'app-navside-works',
   templateUrl: './navside-works.component.html',
-  styleUrls: ['./navside-works.component.scss']
+  styleUrls: ['./navside-works.component.scss'],
 })
 export class NavsideWorksComponent implements OnInit {
-
   @Output() modulo: EventEmitter<Modulo> = new EventEmitter<Modulo>();
-
   /** TEMPORAL */
-
   modelModules: any = [
-  {
-    nombre_module: 'Home',
-    description_module: '',
-    icon_module: this.env.apiUrlIcon()+'cliente.png',
-    permison: 1,
-    state: 1
-  },
-  {
-    nombre_module: 'Cliente',
-    description_module: 'Controla los clientes de la aplicación',
-    icon_module: this.env.apiUrlIcon()+'cliente.png',
-    permison: 1,
-    state: 1
-  },
-  {
-    nombre_module: 'Tienda',
-    description_module: 'Controla las tiendas de la aplicación',
-    icon_module: this.env.apiUrlIcon()+'shop.png',
-    permison: 1,
-    state: 1
-  },
-  {
-    nombre_module: 'Equipo',
-    description_module: 'Controla los equipos de las tiendas',
-    icon_module: this.env.apiUrlIcon()+'machine.png',
-    permison: 1,
-    state: 1
-  },
-  {
-  
-    nombre_module: 'Usuarios',
-    description_module: 'Controla los ususarios de la aplicación',
-    icon_module: this.env.apiUrlIcon()+'usuarios.png',
-    permison: 1,
-    state: 1
-  
-  },
-  // {
-  //   nombre_module: 'Monitoreo de Equipos',
-  //   description_module: 'Monitorea los estados de los equipos adquiridos por tienda ',
-  //   icon_module: this.env.apiUrlIcon()+'cmachine.png',
-  //   permison: 1,
-  //   state: 1
-  // },
-]
+    {
+      nombre_module: 'Home',
+      description_module: '',
+      icon_module: 'home',
+      permison: 1,
+      state: 1,
+    },
+    {
+      nombre_module: 'Cliente',
+      description_module: 'Controla los clientes de la aplicación',
+      icon_module: 'face',
+      permison: 1,
+      state: 1,
+    },
+    {
+      nombre_module: 'Tienda',
+      description_module: 'Controla las tiendas de la aplicación',
+      icon_module: 'storefront',
+      permison: 1,
+      state: 1,
+    },
+    {
+      nombre_module: 'Equipo',
+      description_module: 'Controla los equipos de las tiendas',
+      icon_module: 'room_preferences',
+      permison: 1,
+      state: 1,
+    },
+    {
+      nombre_module: 'Usuarios',
+      description_module: 'Controla los ususarios de la aplicación',
+      icon_module: 'group',
+      permison: 1,
+      state: 1,
+    },
+    // {
+    //   nombre_module: 'Monitoreo de Equipos',
+    //   description_module: 'Monitorea los estados de los equipos adquiridos por tienda ',
+    //   icon_module: this.env.apiUrlIcon()+'cmachine.png',
+    //   permison: 1,
+    //   state: 1
+    // },
+  ];
 
-  constructor(private log: LoginService, private router: Router, private env: Environments) {}
-
-  primary:any;
-  secondary:any;
-  secondary_a:any;
-  secondary_b:any;
+  constructor(
+    private log: LoginService,
+    private router: Router,
+    private env: Environments
+  ) {}
+  primary: any;
+  secondary: any;
+  secondary_a: any;
+  secondary_b: any;
   ngOnInit(): void {
-      this.primary =  this.env.appTheme.colorPrimary;
-      this.secondary = this.env.appTheme.colorSecondary_C;
-      this.secondary_a = this.env.appTheme.colorSecondary_A;
-      this.secondary_b = this.env.appTheme.colorSecondary_B;
-
-      this.obtenerModulos();
+    this.primary = this.env.appTheme.colorPrimary;
+    this.secondary = this.env.appTheme.colorSecondary_C;
+    this.secondary_a = this.env.appTheme.colorSecondary_A;
+    this.secondary_b = this.env.appTheme.colorSecondary_B;
+    this.obtenerModulos();
   }
 
-  obtenerModulos () {
-    //////////console.warn(this.modelModules);
+  obtenerModulos() {
     return this.modelModules;
   }
 
-
-botonClick(data: any) {
-
-  // //////////console.warn(data);
-
+  botonClick(data: any) {
     let modulo: Modulo = {
       nombre: data.nombre_module,
       icono: data.icon_module,
       permiso: data.permison,
       description: data.description_module,
-      state: data.permison
-    }
-
-    localStorage.setItem('modulo',modulo.nombre);
-    localStorage.setItem('iconmodulo',modulo.icono);
-
-    this.modulo.emit(modulo)
-
+      state: data.permison,
+    };
+    localStorage.setItem('modulo', modulo.nombre);
+    localStorage.setItem('iconmodulo', modulo.icono);
+    this.modulo.emit(modulo);
   }
-
 }
