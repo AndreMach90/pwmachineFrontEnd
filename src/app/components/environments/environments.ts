56ac1ec2a13db7420ec2040ebe34e302b227f242
Paolo Rivera <v.humandreams@gmail.com>
2024-06-14 13:35:30 -0500

    overflow tienda

--- src/app/components/environments/environments.ts
+++ src/app/components/environments/environments.ts
@@ -12,7 +12,8 @@ export class Environments {
         
     // apingRok: any = 'http://181.188.224.4:5208';
     // apingRok: any = 'http://192.168.100.12:9095';
-    apingRok:    any = 'http://192.168.55.173:9095';
+    apingRok:    any = 'http://192.168.55.173:9095'; 
+    // apingRok:    any = 'https://c3bc-181-199-63-169.ngrok-free.app'; 
     // apingRok: any = 'https://sfiback.azurewebsites.net';
 
 
