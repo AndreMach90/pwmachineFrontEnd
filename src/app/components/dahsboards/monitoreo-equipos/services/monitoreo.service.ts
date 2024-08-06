d232ef18fa2a0711f7a2ea6248fea13a117abf2a
Andre Paolo Rivera Pintado <v.humandreams@gmail.com>
2024-08-06 17:28:18 -0500

    optimizacion

--- src/app/components/dahsboards/monitoreo-equipos/services/monitoreo.service.ts
+++ src/app/components/dahsboards/monitoreo-equipos/services/monitoreo.service.ts
@@ -1,5 +1,4 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Environments } from 'src/app/components/environments/environments';
 
